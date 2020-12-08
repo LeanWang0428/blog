a973e29061de69a19a055f79217f9b418999b530
LeanWang0428 <983673564@qq.com>
2020-12-08 16:36:23 +0800

    test vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Blog',
+  description: 'Blog',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    sidebar: [
+      '',
+      'about',
+    ],
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about.html' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
